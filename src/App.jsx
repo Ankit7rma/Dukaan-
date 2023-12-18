72558fd0dd807d16b362037b92d15ce167465935
Ankit Sharma <ankitsharma290101@gmail.com>
2023-12-18 21:59:29 +0530

    Layout setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
-const App = () => {
-  return <div>App</div>;
-};
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/home/Home";
+import Order from "./pages/order/Order";
+import Cart from "./pages/cart/Cart";
+import Dashboard from "./pages/admin/dashboard/Dashboard";
+import NoPage from "./pages/noPage/NoPage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/*" element={<NoPage />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
