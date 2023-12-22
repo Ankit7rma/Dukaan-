03630c5abde275dbbd8a1daac631fd2e892f94ba
Ankit Sharma <ankitsharma290101@gmail.com>
2023-12-22 21:52:36 +0530

    Footer+Testimonials+ProductCard+Track

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,18 @@
 import Filter from "../../components/filter/Filter";
 import HeroSection from "../../components/herosection/HeroSection";
 import Layout from "../../components/layout/Layout";
+import ProductCard from "../../components/productCard/ProductCard";
+import Testimonial from "../../components/testimonial/Testmonials";
+import Track from "../../components/track/Track";
 
 const Home = () => {
   return (
     <Layout>
       <HeroSection />
       <Filter />
+      <ProductCard />
+      <Track />
+      <Testimonial />
     </Layout>
   );
 };
