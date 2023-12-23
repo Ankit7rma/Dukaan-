bc8183ae23bdc99376af6659c7f694a8f68bfc71
Ankit Sharma <ankitsharma290101@gmail.com>
2023-12-23 07:25:43 +0530

    Firebase

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,9 +5,32 @@ import ProductCard from "../../components/productCard/ProductCard";
 import Testimonial from "../../components/testimonial/Testmonials";
 import Track from "../../components/track/Track";
 
-const Home = () => {
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, deleteFromCart } from "../../redux/cartSlice";
+
+function Home() {
+  const dispatch = useDispatch();
+  const cartItem = useSelector((state) => state.cart);
+
+  console.log(cartItem);
+
+  const addCart = () => {
+    dispatch(addToCart("shirt"));
+  };
+
+  const deleteCart = () => {
+    dispatch(deleteFromCart("shirt"));
+  };
   return (
     <Layout>
+      <div className="flex gap-5 justify-center">
+        <button className=" bg-gray-300 p-5" onClick={() => addCart()}>
+          add
+        </button>
+        <button className=" bg-gray-300 p-5" onClick={() => deleteCart()}>
+          del
+        </button>
+      </div>
       <HeroSection />
       <Filter />
       <ProductCard />
@@ -15,6 +38,6 @@ const Home = () => {
       <Testimonial />
     </Layout>
   );
-};
+}
 
 export default Home;
